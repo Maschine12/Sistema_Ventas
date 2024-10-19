98bc6e546a8e5d5b3ca728be9e389f3ee376a3e7
PARX <98469650+Maschine12@users.noreply.github.com>
2024-10-19 09:54:31 -0500

    Optimice chartComponent

--- src/components/dashboardComponent/chartComponent.tsx
+++ src/components/dashboardComponent/chartComponent.tsx
@@ -11,13 +11,6 @@ const ChartComponent: React.FC = () => {
   const [utilidadesData, setUtilidadesData] = useState<number[]>([]);
   const [tiempo, setTiempo] = useState<string[]>([]);
 
-  const saveDailyData = (ventas: number, compras: number) => {
-    const today = new Date().toLocaleDateString();
-    const dailyData = { fecha: today, ventas, compras };
-    // Guardar los datos en localStorage o en un API
-    localStorage.setItem('dailyData', JSON.stringify(dailyData));
-  };
-
   const fetchData = async () => {
     try {
       const [ventasRes, comprasRes] = await Promise.all([
@@ -25,45 +18,35 @@ const ChartComponent: React.FC = () => {
         axios.get("../api/compras"),
       ]);
 
-      const comprasOrdenadas = comprasRes.data.sort(
-        (a: { purchaseDate: string }, b: { purchaseDate: string }) =>
-          new Date(a.purchaseDate).getTime() - new Date(b.purchaseDate).getTime()
-      );
-
-      const primeraFecha = new Date(comprasOrdenadas[0].purchaseDate);
-      const hoy = new Date();
       const fechasAcumuladas: Record<string, { ventas: number; compras: number }> = {};
+      const hoy = new Date();
 
-      for (let d = primeraFecha; d <= hoy; d.setDate(d.getDate() + 1)) {
-        const fechaFormateada = d.toLocaleDateString();
-
-        const ventasDelDia = ventasRes.data
-          .filter((venta: { fecha: string }) => new Date(venta.fecha).toLocaleDateString() === fechaFormateada)
-          .reduce((acc: number, venta: { total: number }) => acc + venta.total, 0);
-
-        const comprasDelDia = comprasOrdenadas
-          .filter((compra: { purchaseDate: string }) => new Date(compra.purchaseDate).toLocaleDateString() === fechaFormateada)
-          .reduce((acc: number, compra: { totalAmount: number }) => acc + compra.totalAmount, 0);
-
-        if (!fechasAcumuladas[fechaFormateada]) {
-          fechasAcumuladas[fechaFormateada] = { ventas: 0, compras: 0 };
+      // Procesar las compras
+      comprasRes.data.forEach((compra: { purchaseDate: string; totalAmount: number }) => {
+        const fecha = new Date(compra.purchaseDate).toLocaleDateString();
+        if (!fechasAcumuladas[fecha]) {
+          fechasAcumuladas[fecha] = { ventas: 0, compras: 0 };
         }
-        fechasAcumuladas[fechaFormateada].ventas += ventasDelDia;
-        fechasAcumuladas[fechaFormateada].compras += comprasDelDia;
-      }
+        fechasAcumuladas[fecha].compras += compra.totalAmount;
+      });
+
+      // Procesar las ventas
+      ventasRes.data.forEach((venta: { fecha: string; total: number }) => {
+        const fecha = new Date(venta.fecha).toLocaleDateString();
+        if (!fechasAcumuladas[fecha]) {
+          fechasAcumuladas[fecha] = { ventas: 0, compras: 0 };
+        }
+        fechasAcumuladas[fecha].ventas += venta.total;
+      });
 
       const ventasAcumuladas: number[] = [];
       const comprasAcumuladas: number[] = [];
       const utilidadesAcumuladas: number[] = [];
-      const fechasFormateadas = Object.keys(fechasAcumuladas);
+      let totalVentas = 0, totalCompras = 0;
 
-      let totalVentas = 0;
-      let totalCompras = 0;
-
-      fechasFormateadas.forEach((fecha) => {
+      Object.keys(fechasAcumuladas).forEach(fecha => {
         totalVentas += fechasAcumuladas[fecha].ventas;
         totalCompras += fechasAcumuladas[fecha].compras;
-
         ventasAcumuladas.push(totalVentas);
         comprasAcumuladas.push(totalCompras);
         utilidadesAcumuladas.push(totalVentas - totalCompras);
@@ -72,11 +55,8 @@ const ChartComponent: React.FC = () => {
       setVentasData(ventasAcumuladas);
       setComprasData(comprasAcumuladas);
       setUtilidadesData(utilidadesAcumuladas);
-      setTiempo(fechasFormateadas);
+      setTiempo(Object.keys(fechasAcumuladas));
 
-      // Guardar los últimos datos de ventas y compras al final del día
-      saveDailyData(totalVentas, totalCompras);
-      
     } catch (error) {
       console.error("Error al obtener los datos", error);
     }
@@ -84,115 +64,50 @@ const ChartComponent: React.FC = () => {
 
   useEffect(() => {
     fetchData();
-
-    const dayInMillis = 24 * 60 * 60 * 1000; // Milisegundos en un día
-    const intervalId = setInterval(() => {
-      fetchData();
-    }, dayInMillis);
-
-    return () => clearInterval(intervalId); // Limpiar intervalo al desmontar
+    const intervalId = setInterval(fetchData, 24 * 60 * 60 * 1000);
+    return () => clearInterval(intervalId);
   }, []);
 
   useEffect(() => {
-    const maxValue = Math.max(...ventasData, ...comprasData, ...utilidadesData);
-
     const options = {
       chart: {
         height: "500px",
-        width: "100%",
         type: "line",
         fontFamily: "Inter, sans-serif",
-        toolbar: {
-          show: false,
-        },
+        toolbar: { show: false },
       },
       series: [
-        {
-          name: "Ventas",
-          data: ventasData,
-          color: isDarkMode ? "#007ea7" : "#fb8500",
-        },
-        {
-          name: "Compras",
-          data: comprasData,
-          color: isDarkMode ? "#d62828" : "#023047",
-        },
-        {
-          name: "Utilidades",
-          data: utilidadesData,
-          color: isDarkMode ? "#f77f00" : "#219ebc",
-        },
+        { name: "Ventas", data: ventasData, color: isDarkMode ? "#007ea7" : "#fb8500" },
+        { name: "Compras", data: comprasData, color: isDarkMode ? "#d62828" : "#023047" },
+        { name: "Utilidades", data: utilidadesData, color: isDarkMode ? "#f77f00" : "#219ebc" },
       ],
       xaxis: {
         categories: tiempo,
         labels: {
-          style: {
-            fontFamily: "Inter, sans-serif",
-            cssClass: "text-xs font-normal fill-gray-500 dark:fill-gray-400",
-          },
-        },
-        axisBorder: {
-          show: true,
-        },
-        axisTicks: {
-          show: false,
+          style: { cssClass: "text-xs font-normal fill-gray-500 dark:fill-gray-400" },
         },
       },
-      yaxis: {
-        min: -50,
-        max: maxValue + 20,
-        show: false,
-      },
-      grid: {
-        strokeDashArray: 4,
-        padding: {
-          left: 2,
-          right: 2,
-          top: -26,
-        },
-      },
-      tooltip: {
-        enabled: true,
-        theme: isDarkMode ? "dark" : "light",
-        style: {
-          fontSize: "14px",
-          fontFamily: "Inter, sans-serif",
-          background: isDarkMode ? "dark" : "light",
-          color: isDarkMode ? "dark" : "light",
-        },
-        x: {
-          show: false,
-        },
-      },
-      stroke: {
-        curve: "smooth",
-        width: 6,
-      },
-      dataLabels: {
-        enabled: false,
-      },
-      legend: {
-        show: true,
-      },
+      yaxis: { show: false},
+      tooltip: { enabled: true, theme: isDarkMode ? "dark" : "light" },
+      stroke: { curve: "smooth", width: 6 },
+      dataLabels: { enabled: false },
+      legend: { show: true },
     };
 
     const chartElement = document.getElementById("line-chart");
     if (chartElement) {
       const chart = new ApexCharts(chartElement, options);
       chart.render();
-
-      return () => {
-        chart.destroy();
-      };
+      return () => chart.destroy();
     }
   }, [isDarkMode, ventasData, comprasData, utilidadesData, tiempo]);
 
   return (
     <div className={`${isDarkMode ? "dark" : "light"} w-full h-auto border border-gray-300 rounded-lg shadow p-4 md:p-6`}>
       <div className="flex justify-between mb-5">
-        <div>
-          <p className={`${isDarkMode ? "text-gray-600":"text-gray-700"} text-2xl leading-none font-bold py-2`}>Finanzas</p>
-        </div>
+        <p className={`${isDarkMode ? "text-gray-600" : "text-gray-700"} text-2xl leading-none font-bold py-2`}>
+          Finanzas
+        </p>
       </div>
       <div id="line-chart"></div>
     </div>
